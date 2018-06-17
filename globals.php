d8232b0c5efd51dbee74f3a174716a567f06dc34
Your Name <you@example.com>
2018-06-17 06:09:06 +0200

    start of project

---
+++ globals.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * define path constant
+ */
+define('ROOT',dirname(__FILE__));
+
+define('INCLUDES',ROOT.'/includes');
+define('CONTROLLERS',ROOT.'/controllers');
+define('MODELS',ROOT.'/models');
+define('VIEWS',ROOT.'views');
+
+/**
+ * define spcial constant for project
+ */
+
+define('IN_APP_INCLUDE',true);
+
---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+/**define all config constants*/
+
+/**
+ * DB constant
+ */
+define('SERVER','localhost');
+define('DBNAME','');
+define('DBUSER','root');
+define('DDPASS','');
